1a5d6d79ae4cc9747a973279bdc9adca64050da4
zacti <ll8317727@gmail.com>
2018-02-28 14:50:12 +0800

    done

--- app/mailers/reservation_mailer.rb
+++ app/mailers/reservation_mailer.rb
@@ -5,7 +5,7 @@ class ReservationMailer < ApplicationMailer
 	def booking_email(customer,host)
 		@customer = customer
 		@host = host
-		@url = 'http://localhost:3000'
+		@url = 'https://zacbnb.herokuapp.com/'
 	 	# @reservation_id = reservation_id	
 		mail(to: @customer.email, subject: 'Welcome to My Awesome Site')
 	end
